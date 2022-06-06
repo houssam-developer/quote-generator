d0212efb37b34799956052d34c356bf704800c63
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-06 10:19:28 +0100

    refactor: add switch for all quotes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,18 @@ import './App.scss';
 
 
 function App() {
+	const [displayAllQuotesFlag, setDisplayAllQuotesFlag] = useState(false);
+
 	const quote = {
 		content: 'The first rule of any technology used in a business is that automation applied to an efficient operation will magnify the efficiency. The second is that automation applied to an inefficient operation will magnify the inefficiency.',
 		author: 'Bill Gates'
 	};
+
+	function showAllQuotes(e) {
+		e.preventDefault();
+		console.log('show all quotes');
+		setDisplayAllQuotesFlag(true);
+	}
 
 	return (
 		<div className='max-w-[1024px] mx-auto'>
@@ -20,22 +28,27 @@ function App() {
 			</header>
 
 			<div className='flex flex-col gap-3 border-2 border-blue-400 max-w-[400px] mx-auto'>
-				{/* Single Quote */}
-				<div className='p-4'>
-					<div className="pl-8 border-l-2 md:text-4xl md:border-l-4 border-[#f7df94] ">
-						<div>“{quote.content}”</div>
-					</div>
-				</div>
-				{/* Author */}
-				<button className='quote-author'>
-					<div className='flex flex-col'>
-						<h2 className='quote-author__name hover:text-[#f2f2f2] text-lg md:text-2xl font-semibold self-start'>{quote.author}</h2>
-						<h3 className='quote-author__type hover:text-[#828282] text-sm md:text-lg font-medium self-start'>business</h3>
-					</div>
-					<MdArrowRightAlt fill='#f2f2f2' size={24} />
-				</button>
+				{
+					displayAllQuotesFlag ?
+						<h4>All Quotes</h4>
+						:
+						<>
+							<div className='p-4'>
+								<div className="pl-8 border-l-2 md:text-4xl md:border-l-4 border-[#f7df94] ">
+									<div>“{quote.content}”</div>
+								</div>
+							</div>
+							<button className='quote-author' onClick={showAllQuotes}>
+								<div className='flex flex-col'>
+									<h2 className='quote-author__name hover:text-[#f2f2f2] text-lg md:text-2xl font-semibold self-start'>{quote.author}</h2>
+									<h3 className='quote-author__type hover:text-[#828282] text-sm md:text-lg font-medium self-start'>business</h3>
+								</div>
+								<MdArrowRightAlt fill='#f2f2f2' size={24} />
+							</button>
+						</>
+				}
 			</div>
-		</div>
+		</div >
 	)
 }
 
