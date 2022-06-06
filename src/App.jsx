37f302656d389820a399636d384c8ed1d46974cf
houssam-dev <developpeur.pro.ma@gmail.com>
2022-06-06 10:05:48 +0100

    feat: add single quote with author

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useRef, useState } from 'react';
-import { MdAutorenew } from "react-icons/md";
-
+import { MdAutorenew, MdArrowRightAlt } from "react-icons/md";
+import './App.scss';
 
 
 function App() {
@@ -14,16 +14,26 @@ function App() {
 		<div className='max-w-[1024px] mx-auto'>
 			<header className='p-4'>
 				<button className='ml-auto flex items-center gap-2 p-2 text-[#4f4f4f] hover:bg-slate-200 rounded'>
-					<span className='text-[18px] font-sans font-medium'>random</span>
+					<span className='text-lg font-sans font-medium'>random</span>
 					<MdAutorenew size={20} />
 				</button>
 			</header>
 
-			<div className='p-4'>
-				<div className="pl-8 border-l-2 border-yellow-500 max-w-[400px] mx-auto">
-					{/* Single Quote */}
-					<div>“{quote.content}”</div>
+			<div className='flex flex-col gap-3 border-2 border-blue-400 max-w-[400px] mx-auto'>
+				{/* Single Quote */}
+				<div className='p-4'>
+					<div className="pl-8 border-l-2 md:text-4xl md:border-l-4 border-[#f7df94] ">
+						<div>“{quote.content}”</div>
+					</div>
 				</div>
+				{/* Author */}
+				<button className='quote-author'>
+					<div className='flex flex-col'>
+						<h2 className='quote-author__name hover:text-[#f2f2f2] text-lg md:text-2xl font-semibold self-start'>{quote.author}</h2>
+						<h3 className='quote-author__type hover:text-[#828282] text-sm md:text-lg font-medium self-start'>business</h3>
+					</div>
+					<MdArrowRightAlt fill='#f2f2f2' size={24} />
+				</button>
 			</div>
 		</div>
 	)
